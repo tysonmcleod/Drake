e6cf191103754b04e37d38eec1385d82f2a52ebe
Akram-A <akram_togo@hotmail.com>
2017-02-15 21:54:58 +0100

    Add files via upload

---
+++ projekt/data/menu.js
@@ -0,0 +1,77 @@
+var food = [
+	{
+	"food_name": "Hamburgare",
+	"price": "90kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "http://www.seriouseats.com/recipes/assets_c/2015/07/20150727-teriyaki-burger-recipe-15-thumb-1500xauto-425274.jpg"
+	},
+	{
+	"food_name": "Fiskburgare",
+	"price": "85kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "https://max.se/ImageVaultFiles/id_2557/cf_15/580x325px_Spicy-Chicken_Creole-fiskburgare.jpg"
+	},
+	{
+	"food_name": "Veggoburgare",
+	"price": "80kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "http://lisaoerikshalsoblogg.ellematovin.se/wp-content/uploads/sites/17/2016/02/Veggie-Burger.jpg"
+	},
+	{
+	"food_name": "Halloumiburgare",
+	"price": "95kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "https://img.koket.se/sociallarge/varldens-basta-burgare-2.jpg"
+	},
+		{
+	"food_name": "Test A",
+	"price": "90kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "http://wp.patheos.com.s3.amazonaws.com/blogs/faithwalkers/files/2013/03/bigstock-Test-word-on-white-keyboard-27134336.jpg"
+	},
+	{
+	"food_name": "Test B",
+	"price": "85kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "http://wp.patheos.com.s3.amazonaws.com/blogs/faithwalkers/files/2013/03/bigstock-Test-word-on-white-keyboard-27134336.jpg"
+	},
+	{
+	"food_name": "Test C",
+	"price": "80kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "http://wp.patheos.com.s3.amazonaws.com/blogs/faithwalkers/files/2013/03/bigstock-Test-word-on-white-keyboard-27134336.jpg"
+	},
+	{
+	"food_name": "Test D",
+	"price": "95kr",
+	"lactose": true,
+	"gluten": true,
+	"img_link": "http://wp.patheos.com.s3.amazonaws.com/blogs/faithwalkers/files/2013/03/bigstock-Test-word-on-white-keyboard-27134336.jpg"
+	}
+];
+
+var sauce = [
+	{
+		"sauce_name": "Vitlökssås",
+		"img_link": "http://media.matdelikat.se/2014/03/DSC_3689.jpg"
+	},
+	{
+		"sauce_name": "Kebabsås",
+		"img_link": "http://www.jennysmatblogg.nu/wp-content/uploads/img_6796-1.jpg"
+	},
+	{
+		"sauce_name": "Stark sås",
+		"img_link": "https://res.cloudinary.com/hufennija/image/upload/f_auto,q_50,fl_progressive,w_1200,h_630,c_fill/22831076.jpg"
+	},
+	{
+		"sauce_name": "Mild sås",
+		"img_link": "http://www.matskafferiet.se/wp-content/uploads/2011/11/kebabs%C3%A5s.jpg"
+	},
+]
